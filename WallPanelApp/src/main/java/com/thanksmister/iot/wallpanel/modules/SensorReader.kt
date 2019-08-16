7ad1c36b38f6eac9953c7450735a01e9f37eeac6
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-08-16 23:55:50 +0200

    restore compatibility with single value, inline code
    for sensors that report only single value

--- WallPanelApp/src/main/java/com/thanksmister/iot/wallpanel/modules/SensorReader.kt
+++ WallPanelApp/src/main/java/com/thanksmister/iot/wallpanel/modules/SensorReader.kt
@@ -21,10 +21,10 @@ import android.content.Context.SENSOR_SERVICE
 import android.content.Intent
 import android.content.IntentFilter
 import android.hardware.Sensor
+import android.hardware.Sensor.*
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.hardware.SensorManager.SENSOR_DELAY_NORMAL
 import android.os.BatteryManager
 import android.os.Handler
 import android.util.Log
@@ -35,7 +35,7 @@ import java.util.*
 import javax.inject.Inject
 
 class SensorReader @Inject
-constructor(private val context: Context){
+constructor(private val context: Context) {
 
     private val mSensorManager: SensorManager?
     private val mSensorList = ArrayList<Sensor>()
@@ -44,7 +44,7 @@ constructor(private val context: Context){
     private var callback: SensorCallback? = null
     private var sensorsPublished: Boolean = false
     private var lightSensorEvent: SensorEvent? = null
-    private val lastSensorEvent = mutableMapOf<String,Long?>()
+    private val lastSensorEvent = mutableMapOf<String, Long?>()
 
     private val batteryHandlerRunnable = object : Runnable {
         override fun run() {
@@ -63,7 +63,7 @@ constructor(private val context: Context){
         for (s in mSensorManager.getSensorList(Sensor.TYPE_ALL)) {
             Log.i("TAG", s.toString())
             //if (getSensorName(s.type) != null)
-                mSensorList.add(s)
+            mSensorList.add(s)
         }
     }
 
@@ -84,25 +84,26 @@ constructor(private val context: Context){
         stopSensorReading()
     }
 
-    private fun publishSensorData(sensorName: String?, sensorData: JSONObject) {
+    private fun publishSensorData(sensorName: String, sensorData: JSONObject) {
         Timber.d("publishSensorData")
-        if(sensorName != null) {
-            callback?.publishSensorData(sensorName, sensorData)
-        }
-    }
-/*
-    private fun getSensorName(sensorType: Int): String? {
-        when (sensorType) {
-            Sensor.TYPE_AMBIENT_TEMPERATURE -> return TEMPERATURE
-            Sensor.TYPE_LIGHT -> return LIGHT
-            Sensor.TYPE_MAGNETIC_FIELD -> return MAGNETIC_FIELD
-            Sensor.TYPE_PRESSURE -> return PRESSURE
-            Sensor.TYPE_RELATIVE_HUMIDITY -> return HUMIDITY
-            else -> sensorType.toString()
-        }
-        return null
+        callback?.publishSensorData(sensorName, sensorData)
     }
 
+    private fun getSensorName(sensorType: Int): String {
+        return when (sensorType) {
+            TYPE_ACCELEROMETER -> "accelerometer"
+
+            TYPE_AMBIENT_TEMPERATURE -> "ambient_temperature"
+            TYPE_LIGHT -> "light"
+            TYPE_MAGNETIC_FIELD -> "magnetic_field"
+            TYPE_PRESSURE -> "pressure"
+            TYPE_RELATIVE_HUMIDITY -> "humidity"
+            TYPE_TEMPERATURE -> "temperature"
+            else -> sensorType.toString()
+        }
+    }
+
+    /*
     private fun getSensorUnit(sensorType: Int): String? {
         when (sensorType) {
             Sensor.TYPE_AMBIENT_TEMPERATURE -> return UNIT_C
@@ -111,15 +112,15 @@ constructor(private val context: Context){
             Sensor.TYPE_PRESSURE -> return UNIT_HPA
             Sensor.TYPE_RELATIVE_HUMIDITY -> return UNIT_PERCENTAGE
         }
-        return null
     }
-*/
+    */
+
     /**
      * Start all sensor readings.
      */
     private fun startSensorReadings() {
         Timber.d("startSensorReadings")
-        if(mSensorManager != null) {
+        if (mSensorManager != null) {
             for (sensor in mSensorList) {
                 mSensorManager.registerListener(sensorListener, sensor, updateFrequencyMilliSeconds)
                 Timber.i("SENSOR name: ${sensor.name} vendor: ${sensor.vendor} minDelay=${sensor.minDelay} power=${sensor.power}")
@@ -140,40 +141,65 @@ constructor(private val context: Context){
     private val sensorListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent?) {
             //if(event != null && !sensorsPublished) {
-            if(event != null) {
-                val t = lastSensorEvent.get( event.sensor.name )
+            if (event != null) {
+                val t = lastSensorEvent.get(event.sensor.name)
                 val time = System.currentTimeMillis()
-                if ( t == null || ( time - t ) > updateFrequencyMilliSeconds) {
+                if (t == null || (time - t) > updateFrequencyMilliSeconds) {
                     lastSensorEvent.put(event.sensor.name, time)
-                    if ( t != null ) Log.i("XXX-update", "dt=" +(time - t)
-                            +" ["+event.sensor.name+"] "+event.sensor.type)
+                    if (t != null) Log.i("XXX-update", "dt=" + (time - t)
+                            + " [" + event.sensor.name + "] " + event.sensor.type)
                 } else {
                     return
                 }
                 //Log.i("XXX","timestamp="+event.timestamp.toLong()+ " id="+ event.sensor.id.toInt()+" event.values="+ event.values.size.toString() + event.values[0].toString())
                 //Log.i("XXX","event="+event.toString())
-                var data = JSONObject()
+                val data = JSONObject()
 
                 //data.put(VALUE, event.values[0])
                 data.put("timestamp", event.timestamp)
                 data.put("time", System.currentTimeMillis().toLong())
                 data.put("name", event.sensor.name)
                 data.put("vendor", event.sensor.vendor)
-                //data.put("type", event.sensor.stringType.replace("android.sensor.",""))
+                val sensorTypeName = getSensorName(event.sensor.type)
+                data.put("type", sensorTypeName)
                 data.put("type_id", event.sensor.type)
 
-                var max_values = event.values.size
-                for( i in 0 until max_values ) {
-                    data.put(String.format("v%d",i), event.values[i])
+                var max_values = when (event.sensor.type) {
+                    /*
+                     Table 1. Environment sensors that are supported on the Android platform.
+Sensor 	Sensor event data 	Units of measure 	Data description
+TYPE_AMBIENT_TEMPERATURE 	event.values[0] 	°C 	Ambient air temperature.
+TYPE_LIGHT 	event.values[0] 	lx 	Illuminance.
+TYPE_PRESSURE 	event.values[0] 	hPa or mbar 	Ambient air pressure.
+TYPE_RELATIVE_HUMIDITY 	event.values[0] 	% 	Ambient relative humidity.
+TYPE_TEMPERATURE 	event.values[0] 	°C 	Device temperature.1
 
+
+                     */
+                    TYPE_AMBIENT_TEMPERATURE, TYPE_LIGHT, TYPE_PRESSURE,
+                    TYPE_RELATIVE_HUMIDITY, TYPE_TEMPERATURE -> 1
+                    else -> event.values.size
+                }
+
+                if (max_values == 1) {
+                    data.put(VALUE, event.values[0])
+                } else if (event.sensor.type == TYPE_ACCELEROMETER) {
+                    data.put("x",event.values[0])
+                    data.put("y",event.values[1])
+                    data.put("z",event.values[2])
+                } else {
+                    for (i in 0 until max_values) {
+                        data.put(String.format("v%d", i), event.values[i])
+                    }
                 }
                 //data.put(UNIT, getSensorUnit(event.sensor.type))
                 //data.put(ID, event.sensor.name)
                 //publishSensorData(event.sensor.stringType.replace("android.sensor.",""), data)
-                publishSensorData(event.sensor.type.toString(), data)
+                publishSensorData(sensorTypeName, data)
                 sensorsPublished = true
             }
         }
+
         override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         }
     }
@@ -183,39 +209,28 @@ constructor(private val context: Context){
         Timber.d("getBatteryReading")
         val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         val batteryStatus = context.registerReceiver(null, intentFilter)
-        val batteryStatusIntExtra = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
-        val isCharging = batteryStatusIntExtra == BatteryManager.BATTERY_STATUS_CHARGING || batteryStatusIntExtra == BatteryManager.BATTERY_STATUS_FULL
+        val batteryStatusIntExtra = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+                ?: -1
         val chargePlug = batteryStatus?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) ?: -1
-        val usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB
-        val acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC
         val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
         val data = JSONObject()
         try {
             data.put(VALUE, level)
-            data.put(UNIT, UNIT_PERCENTAGE)
-            data.put(CHARGING, isCharging)
-            data.put(AC_PLUGGED, acCharge)
-            data.put(USB_PLUGGED, usbCharge)
+            data.put(UNIT, "%")
+            data.put("charging", batteryStatusIntExtra == BatteryManager.BATTERY_STATUS_CHARGING)
+            data.put("full", batteryStatusIntExtra == BatteryManager.BATTERY_STATUS_FULL)
+            data.put("acPlugged", chargePlug == BatteryManager.BATTERY_PLUGGED_AC)
+            data.put("usbPlugged", chargePlug == BatteryManager.BATTERY_PLUGGED_USB)
             data.put("time", System.currentTimeMillis().toLong())
         } catch (ex: JSONException) {
             ex.printStackTrace()
         }
 
-        publishSensorData(BATTERY, data)
+        publishSensorData("battery", data)
     }
 
     companion object {
-        const val BATTERY: String = "battery"
-        const val CHARGING: String = "charging"
-        const val AC_PLUGGED: String = "acPlugged"
-        const val USB_PLUGGED: String = "usbPlugged"
-        const val HUMIDITY: String = "humidity"
-        const val LIGHT: String = "light"
-        const val PRESSURE: String = "pressure"
-        const val TEMPERATURE: String = "temperature"
-        const val MAGNETIC_FIELD: String = "magneticField"
         const val UNIT_C: String = "°C"
-        const val UNIT_PERCENTAGE: String = "%"
         const val UNIT_HPA: String = "hPa"
         const val UNIT_UT: String = "uT"
         const val UNIT_LX: String = "lx"
